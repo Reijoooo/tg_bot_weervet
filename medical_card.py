a24e000bbc1e63bddef691bfae7ddec2ab21913a
Reijoooo <griha200142@gmail.com>
2024-09-22 15:32:45 +0700

    pre alfa

--- medical_card.py
+++
@@ -1,6 +0,0 @@
-from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class MedicalCardForm(StatesGroup):
-    chronic_disease = State()
-    allergy = State()
