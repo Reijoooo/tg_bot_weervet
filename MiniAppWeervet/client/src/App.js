87ddcef40491f0100b2f694d6de923daf0c8316d
Reijoooo <griha200142@gmail.com>
2024-10-15 16:38:36 +0700

    123

--- MiniAppWeervet/client/src/App.js
+++ MiniAppWeervet/client/src/App.js
@@ -1,29 +1,36 @@
-// client/src/App.js
-
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [viewportHeight, setViewportHeight] = useState(window.innerHeight); // Управление высотой экрана
 
-  // useEffect(() => {
-  //   // Получение данных с сервера
-  //   fetch('http://localhost:5000/api/message')
-  //     .then(response => response.json())
-  //     .then(data => setMessage(data.message))
-  //     .catch(error => console.error('Ошибка:', error));
-  // }, []);
   useEffect(() => {
-    fetch('/api/message')
-      .then(response => response.json())
-      .then(data => setMessage(data.message))
-      .catch(error => console.error('Ошибка:', error));
+    const tg = window.Telegram.WebApp;
+
+    // Разворачиваем мини-приложение на весь экран
+    tg.expand();
+
+    // Изменение высоты окна при изменении вьюпорта
+    const handleViewportChange = () => {
+      setViewportHeight(tg.viewportHeight);
+      console.log("Изменение вьюпорта. Высота: " + tg.viewportHeight);
+    };
+
+    tg.onEvent('viewportChanged', handleViewportChange);
+
+    return () => {
+      tg.offEvent('viewportChanged', handleViewportChange);
+    };
   }, []);
-  
 
   return (
-    <div className="App">
-      <h1>MiniApp на React и Node.js</h1>
-      <p>{message}</p>
+    <div id="app" style={{ height: `${viewportHeight}px` }}>
+      <h1>Контент приложения</h1>
+      <div className="bottom-buttons">
+        <button className="button">Кнопка 1</button>
+        <button className="button">Кнопка 2</button>
+        <button className="button">Кнопка 3</button>
+        <button className="button">Кнопка 4</button>
+      </div>
     </div>
   );
 }
