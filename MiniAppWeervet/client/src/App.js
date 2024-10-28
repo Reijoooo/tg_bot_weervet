a6d612bc43f812c502f802677a1e12423ef4c58c
Reijoooo <griha200142@gmail.com>
2024-10-28 11:42:43 +0700

    1

--- MiniAppWeervet/client/src/App.js
+++ MiniAppWeervet/client/src/App.js
@@ -5,13 +5,6 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [message, setMessage] = useState('');
 
-  // useEffect(() => {
-  //   // Получение данных с сервера
-  //   fetch('http://localhost:5000/api/message')
-  //     .then(response => response.json())
-  //     .then(data => setMessage(data.message))
-  //     .catch(error => console.error('Ошибка:', error));
-  // }, []);
   useEffect(() => {
     fetch('/api/message')
       .then(response => response.json())
@@ -19,6 +12,10 @@ function App() {
       .catch(error => console.error('Ошибка:', error));
   }, []);
   
+  <form action={search}>
+    <input name="query" />
+    <button type="submit">Search</button>
+</form>
 
   return (
     <div className="App">
